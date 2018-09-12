2e0015f24880f2c12f78c786ac05f2f5c77431b0
Clebert Suconic <clebertsuconic@apache.org>
2018-09-12 17:13:11 -0400

    removing JIRA REST for this report

--- artemis-2.6.x-gap.sh
+++ artemis-2.6.x-gap.sh
@@ -2,4 +2,4 @@ export CHECKOUT=$1
 export OUTPUT=$2
 export START=5e5ae404174d61735cf5283b59da59460c36d629
 
-java -jar ./target/jira-git-0.1.SNAPSHOT-jar-with-dependencies.jar artemis $CHECKOUT $OUTPUT $START master true 2.6.x 2.6.0
+java -jar ./target/jira-git-0.1.SNAPSHOT-jar-with-dependencies.jar artemis $CHECKOUT $OUTPUT $START master false 2.6.x 2.6.0
